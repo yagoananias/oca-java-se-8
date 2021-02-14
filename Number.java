98a511a671196919b0f2ff0f070c58b9c63ac2cb
Yago Ananias <yago.crazy@hotmail.com>
2021-02-14 15:53:33 -0300

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,3 +1,15 @@
 public class Number {
-
+  int number;
+  
+  public Number(int number) {
+    this.number = number;
+  }
+  
+  int getNumber() {
+    return this.number;
+  }
+  
+  void setNumber(int number) {
+    this.number = number;
+  }
 }
