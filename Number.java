fd59be3e2b48784afc976c60cde323fe4b819fe1
Yago Ananias <yago.crazy@hotmail.com>
2021-02-14 16:01:58 -0300

    Update Number.java

--- Number.java
+++ Number.java
@@ -14,8 +14,14 @@ public class Number {
   }
 }
 
+//Metodo a ser chamado
 void addThree(Number value) {
   System.out.println("Parameter: value = " + value.getNumber());
   value.setNumber(value.getNumber() + 3);
   System.out.println("Leaving the method: value = " value.getNumber());
 }
+
+Number value = new Number(1);
+System.out.println("Argument: value = " + value.getNumber());
+addThree(value);
+System.out.println("After method call: value = " + value.getNumber());
