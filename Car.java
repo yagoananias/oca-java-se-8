e8e4587345f34e93a30a1f47346a8363153c6286
Yago Ananias <yago.crazy@hotmail.com>
2021-02-22 13:21:01 -0300

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,2 +1,12 @@
 public class Car {
+  int topSpeed;
+  boolean running;
+  Car (int topSpeed, boolean running) {
+    this.running = running;
+    this.topSpeed = topSpeed;
+  }
+  
+  public boolean isRunning() {
+    return running;
+  }
 }
